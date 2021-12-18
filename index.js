29d551dc5db6941fbfd527261faf6845f3031175
Tokmianin Alex <tokafrag@ya.ru>
2021-12-18 15:20:46 +0500

    Базовый Express.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path =require('path')
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const coursesRoutes = require('./routes/courses')
+const addRoutes = require('./routes/add')
+
+const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main', extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/add', addRoutes)
+app.use('/card', cardRoutes)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
